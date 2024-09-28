cf07b4cb2c79022bba37bd55b4b5d359dabcf854
Maaz Nadeem <maaznadeem007@outlook.com>
2024-09-28 23:54:40 +0500

    JS Practice

--- script.js
+++ script.js
@@ -22,4 +22,130 @@
 
 // console.log("Hello I can run JS through my browser's console");
 
+// Assignment 2 
+
+// Question  1
+// let username = "Maaz"
+
+// Question 2 
+
+// let MyName = "Maaz Nadeem";
+
+// // Question 3
+// let message;
+
+// message = "Hello World";
+
+// alert(message);
+
+// // Question 4
+// let StudentName = "Maaz Nadeem";
+// let StudentAge = "22 years old";
+// let StudentCourse = "Certified Mobile Application Development";
+
+// alert(StudentName);
+// alert(StudentAge);
+// alert(StudentCourse);
+
+// Question 5 
+
+// let pizzaMessage = "PIZZA\nPIZZ\nPIZ\nPI\nP";
+
+// alert(pizzaMessage);
+
+// Question 6
+
+// let Email = "example@example.com";
+
+// let EmailMessage = "My email address is " + Email;
+
+// alert(EmailMessage);
+
+// Question 7
+
+// let book = "A smarter way to learn JavaScript";
+
+// let bookMessage = "I am trying to learn from the Book. " + book;
+
+// alert(bookMessage);
+
+// Question 8
+// document.write("Yah! I can write HTML content through JavaScript");
+
+// Question 9
+// let specialString = "“————ஜ۩۞۩ஜ————”";
+
+//     alert(specialString);
+//     document.write(specialString);
+
+// Assignment 3
+
+// // Question 1
+//     let age = 22;
+//     // alert("I am " + age + " years old");
+
+// Question 2
+
+// let birthYear = 2002;
+//         alert("My birth year is " + birthYear);
+
+// Question 3
+// let visitorName = "Maaz";
+// let productTitle = "T-shirt"; 
+// let quantity = 5; 
+
+
+// let message = visitorName + " ordered " + quantity + " " + productTitle + "(s) on XYZ Clothing store";
+
+// document.write(message);
+
+// Question 4
+// let firstName = "Maaz", age = 22, isStudent = true;
+
+// Question 5 
+// 5  LEGAL VARIABLES
+// let myName = "Maaz";
+// let $price = 100;
+// let _age = 22;
+// let user123 = "Nadeem";
+// let CamelCaseVar = true;
+
+
+// 5 ILLEGAL VARIABLE
+// let 1user = "Invalid";
+// let my-name = "Invalid"; 
+// let var = "Invalid"; 
+// let user name = "Invalid";
+// let %age = 20;
+
+// Question 6
+// let heading = "Rules for naming JS variables";
+// let rule1 = "Variable names can only contain letters, numbers, $, and _. For example: $my_1stVariable";
+// let rule2 = "Variables must begin with a letter, $, or _. For example: $name, _name, or name";
+// let rule3 = "Variable names are case sensitive";
+// let rule4 = "Variable names should not be JS keywords";
+
+// document.write("<h1>" + heading + "</h1>");
+// document.write("<p>" + rule1 + "</p>");
+// document.write("<p>" + rule2 + "</p>");
+// document.write("<p>" + rule3 + "</p>");
+// document.write("<p>" + rule4 + "</p>");
+
+// Question 7
+// let ticketPrice = 600;
+
+// let totalCost = ticketPrice * 5;
+
+// document.write(`Total cost to buy 5 tickets to a movie is ${totalCost} PKR`);
+
+// Question 8
+
+// let number = 4;
+
+// document.write(`Table of ${number} <br>`);
+// for(let i = 1; i <= 10; i++) {
+//     document.write(`${number} x ${i} = ${number * i} <br>`);
+// }
+
+
 
