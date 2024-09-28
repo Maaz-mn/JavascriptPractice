1bb5024066e84dc516175a7c112986c71e6bc577
Maaz Nadeem <maaznadeem007@outlook.com>
2024-09-28 23:08:57 +0500

    js

---
+++ script.js
@@ -0,0 +1,25 @@
+// Assigment 1
+// Question 1
+
+
+// window.alert("Welcome to our website! We hope you have a great experience.")
+
+// Question 2
+    // window.alert("Error! Please enter a valid password.")
+
+// Question 3 
+
+// window.alert("Welcome to JS Land...\nHappy Coding!");
+
+// Question 4
+
+//  window.alert
+//     alert("Welcome to JS Land...");
+    
+//     alert("Happy Coding!");
+
+// Question 5
+
+// console.log("Hello I can run JS through my browser's console");
+
+
